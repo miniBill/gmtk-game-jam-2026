2c160cea673fb751a489eb3b2e368d7a92e5f316
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-07-24 09:37:25 +0200

    Putting down a skeleton

---
+++ Attribution.md
@@ -0,0 +1,11 @@
+## Seamless Space Backgrouds
+
+By Screaming Brain Studio
+
+https://screamingbrainstudios.itch.io/seamless-space-backgrounds
+
+## Interface Bleeps
+
+By Bleeoop
+
+https://bleeoop.itch.io/interface-bleeps
--- src/Main.elm
+++ src/Main.elm
@@ -1,46 +1,170 @@
-module Main exposing (main)
+port module Main exposing (main)
 
-import Browser
+import Audio exposing (Audio, AudioCmd, AudioData)
 import Html exposing (Html)
+import Html.Attributes
+import Html.Events
+import Json.Decode
+import Json.Encode
+import Quantity
+import Random
+import Task
+import Time
 
 
 type alias Model =
-    {}
+    { page : Page
+    , sound : Maybe ( Audio.Source, Time.Posix )
+    }
+
+
+type Page
+    = Menu
+    | Playing PlayingModel
+
+
+type alias PlayingModel =
+    { initialSeed : Int
+    , currentSeed : Random.Seed
+    }
 
 
 type Msg
-    = Noop
+    = Play
+    | TimeResult Audio.Source Time.Posix
+    | InitialSeed Int
+    | PlaySound
+    | AudioLoadResult (Result Audio.LoadError Audio.Source)
 
 
-main : Program () Model Msg
+main : Program () (Audio.Model Msg Model) (Audio.Msg Msg)
 main =
-    Browser.element
+    Audio.elementWithAudio
         { init = init
         , view = view
         , update = update
         , subscriptions = subscriptions
+        , audio = audio
+        , audioPort =
+            { fromJS = audioPortFromJS
+            , toJS = audioPortToJS
+            }
         }
 
 
-init : () -> ( Model, Cmd Msg )
+port audioPortToJS : Json.Encode.Value -> Cmd msg
+
+
+port audioPortFromJS : (Json.Decode.Value -> msg) -> Sub msg
+
+
+audio : AudioData -> Model -> Audio
+audio audioData model =
+    case model.sound of
+        Nothing ->
+            Audio.silence
+
+        Just ( source, at ) ->
+            Audio.audioWithConfig
+                { loop = Nothing
+                , playbackRate = 1
+                , startAt = Quantity.zero
+                }
+                source
+                at
+
+
+init : () -> ( Model, Cmd Msg, AudioCmd Msg )
 init () =
-    ( {}, Cmd.none )
+    ( { page = Menu
+      , sound = Nothing
+      }
+    , Cmd.none
+    , Audio.cmdNone
+    )
 
 
-view : Model -> Html Msg
-view model =
+view : AudioData -> Model -> Html Msg
+view audioData model =
     Html.main_
-        []
-        []
+        [ Html.Attributes.style "display" "flex"
+        , Html.Attributes.style "gap" "8px"
+        ]
+        (case model.page of
+            Menu ->
+                [ Html.button
+                    [ Html.Events.onClick Play
+                    ]
+                    [ Html.text "Play game" ]
+
+                -- , Html.button
+                --     [ Html.Events.onClick PlaySound
+                --     ]
+                --     [ Html.text "Play sound" ]
+                ]
+
+            Playing playingModel ->
+                viewPlaying audioData model playingModel
+        )
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Noop ->
-            ( model, Cmd.none )
+viewPlaying : AudioData -> Model -> PlayingModel -> List (Html Msg)
+viewPlaying audioData model playingModel =
+    [ Html.text "TODO" ]
 
 
-subscriptions : Model -> Sub Msg
-subscriptions _ =
+update : AudioData -> Msg -> Model -> ( Model, Cmd Msg, AudioCmd Msg )
+update audioData msg model =
+    case
+        -- Debug.log "msg"
+        msg
+    of
+        PlaySound ->
+            case model.sound of
+                Nothing ->
+                    ( model
+                    , Cmd.none
+                    , Audio.loadAudio AudioLoadResult "/media/Interface_Bleeps_OGG/Bleep_05.ogg"
+                    )
+
+                Just ( sound, _ ) ->
+                    ( model
+                    , Time.now |> Task.perform (TimeResult sound)
+                    , Audio.cmdNone
+                    )
+
+        Play ->
+            ( model
+            , Random.int 0 Random.maxInt |> Random.generate InitialSeed
+            , Audio.cmdNone
+            )
+
+        InitialSeed initialSeed ->
+            ( { model
+                | page =
+                    Playing
+                        { initialSeed = initialSeed
+                        , currentSeed = Random.initialSeed initialSeed
+                        }
+              }
+            , Cmd.none
+            , Audio.cmdNone
+            )
+
+        AudioLoadResult (Err e) ->
+            let
+                _ =
+                    Debug.log "AudioLoadResult (Err e)" e
+            in
+            ( model, Cmd.none, Audio.cmdNone )
+
+        AudioLoadResult (Ok sound) ->
+            ( model, Time.now |> Task.perform (TimeResult sound), Audio.cmdNone )
+
+        TimeResult sound now ->
+            ( { model | sound = Just ( sound, now ) }, Cmd.none, Audio.cmdNone )
+
+
+subscriptions : AudioData -> Model -> Sub Msg
+subscriptions _ _ =
     Sub.none
